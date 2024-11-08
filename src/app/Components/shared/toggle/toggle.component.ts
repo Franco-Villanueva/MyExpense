700fc8de92a53e1fcf76526aeb96f8850f69d8aa
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-11-08 06:55:58 -0300

    navbar con botones y title

--- src/app/Components/shared/toggle/toggle.component.ts
+++ src/app/Components/shared/toggle/toggle.component.ts
@@ -1,16 +1,38 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import { FormsModule } from '@angular/forms';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-toggle',
   standalone: true,
-  imports: [MatSlideToggleModule, FormsModule],
+  imports: [MatSlideToggleModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './toggle.component.html',
-  styleUrl: './toggle.component.css'
+  styleUrls: ['./toggle.component.css']
 })
-export class ToggleComponent {
-  title = 'toggle';
+export class ToggleComponent implements OnInit {
+  darkMode: boolean = false;
 
+  ngOnInit(): void {
+    // Recuperar el estado del modo oscuro desde el localStorage al iniciar
+    const savedTheme = localStorage.getItem('darkMode');
+    if (savedTheme) {
+      this.darkMode = JSON.parse(savedTheme);
+      this.applyTheme();
+    }
+  }
+
+  toggleTheme(): void {
+    // Cambiar el tema y guardarlo en localStorage
+    this.darkMode = !this.darkMode;
+    localStorage.setItem('darkMode', JSON.stringify(this.darkMode));
+    this.applyTheme();
+  }
+
+  applyTheme(): void {
+    if (this.darkMode) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
 }
