3bb8a2d211d5271934e031b8944fd9369e0d5110
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-11-08 20:12:31 -0300

    avances dashboard

---
+++ src/app/Components/grid-list/grid-list.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+interface Box {
+  icon: string;
+  text: string;
+  valor: number;
+  sig: string;
+}
+
+interface Month {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-grid-list',
+  standalone: true,
+  imports: [MatGridListModule, MatFormFieldModule, MatSelectModule, MatInputModule],
+  templateUrl: './grid-list.component.html',
+  styleUrl: './grid-list.component.css'
+})
+export class GridListComponent {
+
+  
+
+  list: Box[] = [
+    {text: 'Balance Total', icon:'x', valor: 1500 , sig:'$'},
+    {text: 'Ingreso', icon:'x', valor: 1500, sig:'+$'},
+    {text: 'Egreso', icon:'x', valor: 1500, sig:'-$'},
+  ];
+
+  months: Month[] = [
+    { value: 'enero', viewValue: 'Enero' },
+    { value: 'febrero', viewValue: 'Febrero' },
+    { value: 'marzo', viewValue: 'Marzo' },
+    { value: 'abril', viewValue: 'Abril' },
+    { value: 'mayo', viewValue: 'Mayo' },
+    { value: 'junio', viewValue: 'Junio' },
+    { value: 'julio', viewValue: 'Julio' },
+    { value: 'agosto', viewValue: 'Agosto' },
+    { value: 'septiembre', viewValue: 'Septiembre' },
+    { value: 'octubre', viewValue: 'Octubre' },
+    { value: 'noviembre', viewValue: 'Noviembre' },
+    { value: 'diciembre', viewValue: 'Diciembre' },
+  ];
+  
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { ToggleComponent } from "./Components/shared/toggle/toggle.component";
+import { GridListComponent } from "./Components/grid-list/grid-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, ToggleComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, ToggleComponent, GridListComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
